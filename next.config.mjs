4cc311466d3a0a66f2394a0dac923d055b301a49
Alciney Nunes <alcineynunes790@gmail.com>
2026-02-28 22:00:29 -0300

    fix(vercel): include prisma artifacts in output tracing

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: ["@prisma/client", "prisma"],
+    outputFileTracingIncludes: {
+      "/*": [
+        "./node_modules/.prisma/master/**/*",
+        "./node_modules/.prisma/tenant/**/*",
+        "./node_modules/.prisma/client/**/*",
+        "./node_modules/@prisma/**/*",
+        "./prisma/schema.prisma",
+        "./prisma/tenant/schema.prisma",
+      ],
+    },
+  },
+};
 
 export default nextConfig;
